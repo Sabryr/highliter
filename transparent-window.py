6f237cc68a2a0d5b5cddc66823bbcd8b81093fe6
Sabry Razick <sabryr@gmail.com>
2020-11-01 22:24:41 +0100

    Yellow

--- transparent-window.py
+++ transparent-window.py
@@ -1,5 +1,15 @@
 from tkinter import Tk # or(from Tkinter import Tk) on Python 2.x
-root = Tk()
+root = Tk(className='Highlight')
+root.geometry("400x200")
+root.configure(bg='yellow')
 root.wait_visibility(root)
 root.wm_attributes('-alpha',0.3)
 root.mainloop()
+
+#from tkinter import *
+
+# set window size
+
+#set window color
+
+#gui.mainloop() 
