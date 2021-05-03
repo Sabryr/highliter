a8063764871e5c0181d08d44f48c618873e944d4
Sabry Razick <sabryr@gmail.com>
2021-05-03 12:34:45 +0200

    Type or not tranparent window

---
+++ type-or-not.py
@@ -0,0 +1,17 @@
+from tkinter import Tk # or(from Tkinter import Tk) on Python 2.x
+root = Tk(className='Highlight')
+root.geometry("400x200")
+root.configure(bg='yellow')
+root.wait_visibility(root)
+root.wm_attributes('-alpha',0.5)
+entry1=Tk.Entry(root)
+root.mainloop()
+
+
+#from tkinter import *
+
+# set window size
+
+#set window color
+
+#gui.mainloop() 
